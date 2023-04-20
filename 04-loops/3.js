1066a4e65d36eb6c8063cdb62fba12907e12b792
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-04-20 13:57:35 +0300

    1

--- 04-loops/3.js
+++ 04-loops/3.js
@@ -7,7 +7,7 @@
 <li>Используй циклы</li> */
 
 for (let i = 1; i <= 5; i += 1) {
-  console.log("\n=> " + i + " <=\n\n");
+  console.log("=> " + i + " <=");
   for (let j = 1; j <= 10; j += 1) {
     console.log(i + " x " + j + " = " + i * j);
   }
