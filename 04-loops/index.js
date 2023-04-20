defe2c11a9f59903e0c08a003be3392b5ca59830
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-04-20 13:07:22 +0300

    1

--- 04-loops/index.js
+++ 04-loops/index.js
@@ -1,16 +1,6 @@
-function run() {
-    var a = 0;
-  
-    if (a == 1) {
-      return c;
-    }
-  
-    for (let i = 1; i < 10; i++) {
-      console.log(i);
-    }
-  
-    return (d = a);
+for (let i = 1; i <= 5; i += 1) {
+  console.log("\n=> " + i + " <=\n\n");
+  for (let j = 1; j <= 10; j += 1) {
+    console.log(i + " x " + j + " = " + i * j);
   }
-  
-  run();
-  
+}
