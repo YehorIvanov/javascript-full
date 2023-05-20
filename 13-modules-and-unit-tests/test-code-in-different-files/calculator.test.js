9f37c7323bb0a453a0326df1bd5db1e535f028a2
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-05-20 17:30:40 +0300

    1

--- 13-modules-and-unit-tests/test-code-in-different-files/calculator.test.js
+++ 13-modules-and-unit-tests/test-code-in-different-files/calculator.test.js
@@ -1,3 +1,8 @@
-import { getEvenNumbers } from "calculator.js";
+import { getSquaredArray} from "./calculator.js";
+import { getOddNumbers } from "calculator.js";
+import getSum from "calculator.js";
 
-it("expect get squared array", () => {});
+it("expect get squared array", () => {
+    const result = getSquaredArray([1,2,3,]);
+    expect(result).toEqual([1,4,9,])
+});
