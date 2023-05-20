d8d0b6aec732f9f38d489284c749ddc97bdb75ca
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-05-20 17:44:31 +0300

    1

--- 13-modules-and-unit-tests/test-code-in-different-files/calculator.test.js
+++ 13-modules-and-unit-tests/test-code-in-different-files/calculator.test.js
@@ -1,6 +1,4 @@
-import { getOddNumbers } from "./calculator.js";
-import getSum from "./calculator.js";
-import { getSquaredArray } from "./calculator.js";
+import getSum, { getOddNumbers, getSquaredArray } from "./calculator.js";
 
 it("expect get squared array", () => {
   const result = getSquaredArray([1, 2, 3]);
