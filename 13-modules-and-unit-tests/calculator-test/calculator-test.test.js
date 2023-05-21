ed3fdb688130493dee0087b0161843dd538d8e40
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-05-21 14:28:42 +0300

    calculator test

---
+++ 13-modules-and-unit-tests/calculator-test/calculator-test.test.js
@@ -0,0 +1,31 @@
+import { calc } from "./calculator-test";
+
+it("get 2 + 2", () => {
+  const result = calc("2 + 2");
+  expect(result).toEqual("2 + 2 = 4");
+});
+
+it("get 2 * 2", () => {
+  const result = calc("2 * 2");
+  expect(result).toEqual("2 * 2 = 4");
+});
+
+it("get 2 / 2", () => {
+  const result = calc("2 / 2");
+  expect(result).toEqual("2 / 2 = 1");
+});
+
+it("get 2 - 2", () => {
+  const result = calc("2 - 2");
+  expect(result).toEqual("2 - 2 = 0");
+});
+
+it("undefinded operator", () => {
+  const result = calc("2 % 2");
+  expect(result).toEqual("2 % 2 = undefined");
+});
+
+it("not a string", () => {
+  const result = calc(4);
+  expect(result).toEqual(null);
+});
