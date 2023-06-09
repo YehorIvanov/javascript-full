adb1a5f5b216e699cad37ecb01d390f2328755d8
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-06-09 17:20:53 +0300

    dom

--- 13-modules-and-unit-tests/unit-tests-for-previous-tasks/index.js
+++ 13-modules-and-unit-tests/unit-tests-for-previous-tasks/index.js
@@ -22,3 +22,73 @@ export const getAdults = (obj) => {
   }
   return resultingObj;
 };
+
+const createLogger = () => {
+  const logMassageArray = [];
+  const warn = (message) => {
+    logMassageArray.push({
+      message,
+      dateTime: new Date(),
+      type: "warn",
+    });
+  };
+  const error = (message) => {
+    logMassageArray.push({
+      message,
+      dateTime: new Date(),
+      type: "error",
+    });
+  };
+  const log = (message) => {
+    logMassageArray.push({
+      message,
+      dateTime: new Date(),
+      type: "log",
+    });
+  };
+  const getRecords = (type) => {
+    if (type !== undefined)
+      return logMassageArray
+        .filter((elem) => elem.type === type)
+        .sort((a, b) => b.dateTime - a.dateTime);
+    return logMassageArray.sort((a, b) => b.dateTime - a.dateTime);
+  };
+  return {
+    warn,
+    error,
+    log,
+    getRecords,
+  };
+};
+
+// put your code here
+export const timer = {
+  secondsPassed: 0,
+  minsPassed: 0,
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      if (this.secondsPassed < 60) {
+        this.secondsPassed += 1;
+      } else {
+        this.minsPassed += 1;
+        this.secondsPassed = 0;
+      }
+    }, 1000);
+  },
+  getTime() {
+    function getSeconds(secondsPassed) {
+      if (secondsPassed < 10) {
+        return secondsPassed;
+      }
+      return `0${secondsPassed}`;
+    }
+    return `${this.minsPassed}:${getSeconds(this.secondsPassed)}`;
+  },
+  stopTimer() {
+    clearInterval(this.intervalId);
+  },
+  resetTimer() {
+    this.secondsPassed = 0;
+    this.minsPassed = 0;
+  },
+};
