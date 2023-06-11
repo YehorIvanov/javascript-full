f48d5ea8b8bf3da7b86b034b2f579063a74b8b46
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-06-11 13:48:15 +0300

    DOM 1

--- 21-dom/to-do-list-render-list/index.js
+++ 21-dom/to-do-list-render-list/index.js
@@ -6,11 +6,26 @@ const tasks = [
   { text: "Buy meat", done: true },
 ];
 
-
 /**
  * @param {object[]} tasksList
  * @return {undefined}
  */
 const renderTasks = (tasksList) => {
-  // put your code here
+  const listElem = document.querySelector(".list");
+  const listItemElems = tasksList.map(({ text, done }) => {
+    const listItemElem = document.createElement("li");
+    listItemElem.classList.add("list__item");
+    if (done) {
+      listItemElem.classList.add("list__item_done");
+    }
+    const checkboxElem = document.createElement("input");
+    checkboxElem.setAttribute("type", "checkbox");
+    checkboxElem.checked = done;
+    checkboxElem.classList.add("list__item-checkbox");
+
+    listItemElem.append(checkboxElem, text);
+    return listItemElem;
+  });
+  listElem.append(...listItemElems);
 };
+renderTasks(tasks);
