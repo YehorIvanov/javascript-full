ef49e50d91097e94a0c61ce19a42bcb42141b1c1
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-06-11 14:41:12 +0300

    dom 4

--- 21-dom/get-som-element/index.js
+++ 21-dom/get-som-element/index.js
@@ -5,11 +5,12 @@
 // <li>Напишите ф-цию <code>getTitleElement</code>, которая найдет на странице элемент с классом <code>.title</code>, выведет найденый элемент в консоль с помощью <code>console.dir(titleElem)</code> и вернет его</li>
 // <li>Напишите ф-цию <code>getInputElement</code>, которая найдет на странице input c типом <code>text</code> выведет найденый элемент в консоль с помощью <code>console.dir(inputElem)</code> и вернет его</li>
 // <li>Ф-ции должны быть в файле <code>index.js</code> и экспортироваться из него под своими именами</li>
-export const gerTitleElement = () => {
+
+export function getTitleElement() {
   const titleElem = document.querySelector(".title");
   console.dir(titleElem);
   return titleElem;
-};
+}
 
 export const getInputElement = () => {
   const inputElem = document.querySelector("input");
