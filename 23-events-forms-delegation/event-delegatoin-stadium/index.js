cae16c9c08cfc1c825001d4d31dc3ebadb55743b
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-06-20 12:56:51 +0300

    event delegation stadium

--- 23-events-forms-delegation/event-delegatoin-stadium/index.js
+++ 23-events-forms-delegation/event-delegatoin-stadium/index.js
@@ -6,9 +6,40 @@
 // <li>По клику мыши по месту, должны отобразиться координаты места в отведенном для этого блоке. Формат текста <code>S 2 - L 7 - S 3</code>. Cледи за пробелами</li>
 // <li>Секторы - CSS класс <code>.sector</code>, сидения - CSS класс <code>.sector__seat</code>, табло - CSS класс <code>.board</code>
 
-const arenaElem = document.querySelector(".arena");
-console.log(arenaElem);
-const sectorElem = document.createElement("div");
-sectorElem.classList.add("sector");
-console.log(sectorElem);
-arenaElem.innerHTML = sectorElem;
+// const arenaElem = document.querySelector(".arena");
+
+// const sectorLineElem = document.createElement("div");
+// sectorLineElem.classList.add("sector__line");
+
+// sectorElem.classList.add("sector");
+const showSeatNumber = (event) => {
+  console.log(event.target.dataset);
+  document.querySelector(".board__selected-seat").innerHTML =
+    event.target.dataset.seat;
+};
+
+const arenaRender = () => {
+  for (let sector = 1; sector < 4; sector += 1) {
+    console.log(sector);
+    const sectorElem = document.createElement("div");
+    sectorElem.classList.add("sector");
+    for (let line = 1; line < 11; line += 1) {
+      const sectorLineElem = document.createElement("div");
+      sectorLineElem.classList.add("sector__line");
+      for (let seat = 1; seat < 11; seat += 1) {
+        const seatElem = document.createElement("div");
+        seatElem.classList.add("sector__seat");
+        seatElem.addEventListener("mouseover", showSeatNumber);
+        seatElem.dataset.seat = `S ${sector} - L ${line} - S ${seat}`;
+        sectorLineElem.appendChild(seatElem);
+      }
+      sectorElem.appendChild(sectorLineElem);
+    }
+    document.querySelector(".arena").appendChild(sectorElem);
+  }
+};
+
+arenaRender();
+
+// sectorLineElem.appendChild(seatElem);
+// sectorElem.appendChild(sectorLineElem);
