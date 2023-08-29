43e3964cd1b4a70c8935d31c7d1f4b020e2c1959
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-08-29 19:28:31 +0300

    27-todo-list

--- 27-local-storage/to-do-list/src/scripts/index.js
+++ 27-local-storage/to-do-list/src/scripts/index.js
@@ -1,7 +1,10 @@
 import renderTasks from "./render.js";
 import addTaskAction from "./createTask.js";
+import { getItem, setItem } from "./storage.js";
 
 window.addEventListener("DOMContentLoaded", () => {
+  if (!Array.isArray(getItem("tasksList"))) setItem("tasksList", []);
+
   renderTasks();
 
   document
