70296cf30f15d7c371e8cfa12bfb2a77e57a0e03
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-10-03 18:48:33 +0300

    sgsrh

---
+++ moment/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import moment from "moment";
+
+// const timeElem = document.querySelector(".time");
+// timeElem.innerHTML = moment().format();
+// const time = moment();
+// // time.locale("uk");
+// console.log(moment().locale());
+console.log(moment().format("llll"));
+// const time = moment();
